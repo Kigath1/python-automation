d69d609546c7cab9714f246b6bd90fdf9aa713eb
Kigath1 <dennismm502@gmail.com>
2025-05-01 09:57:32 +0300

    news api info saved to file

--- API's/main.py
+++ API's/main.py
@@ -1,16 +1,25 @@
-import requests 
-import json 
+# in this file we are going to use the newsapi.org API to get the latest news articles related to a specific topic and save them to a text file.
+import requests
 
-url = 'https://newsapi.org/v2/everything?qInTitle=united%20states&from=2025-2-27&to=2025-2-28&sortBy=popularity&language=en&apiKey=49e3cc5576ec4c26bcf157ee45e712cc'
+def url_details(topic, from_date, to_date, api_key='890603a55bfa47048e4490069ebee18c'): 
+    url=f'https://newsapi.org/v2/everything?qInTitle={topic}&from={from_date}&to={to_date}&sortBy=popularity&language=en&apiKey={api_key}'
 
-content = requests.get(url).json()
-print(content)
-# print(content["articles"][0]["content"])
+    results = requests.get(url)
+    content = results.json()
+    articles = content.get('articles',[])
+    results = []
 
-# f_content = json.dumps(content, indent=4)
-# print(f_content)
+    for article in articles: 
+        results.append(f"TITLE\n{article['title']}\nDESCRIPTION\n{article['description']}\n")
 
-# txt_file = '../output_files/news.txt'
-# with open (txt_file, 'w') as code_in_file:
-#     code_in_file.write(f_content) 
+    return results
+
+article_results = url_details('stock market', '2025-04-27', '2025-04-28')
+for entry in article_results:
+    print(entry)
+
+filename = "../output_files/stock_market.txt"
+with open(filename, 'w', encoding='utf-8') as file: 
+    for entry in article_results:
+        file.write(entry + "\n")
 
